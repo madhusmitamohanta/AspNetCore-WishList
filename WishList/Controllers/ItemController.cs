26dafa49a31c0e3607da38ce42a889fb86d4e197
madhusmitamohanta <madhusmita.mohanta2@mindtree.com>
2020-07-10 20:30:40 +0000

    Completed module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Item> model = _context.Items.ToList();
+            return View("Index",model);
+        }
+
+        [HttpGet]
+        public IActionResult create()
+        {
+            return View("create");
+        }
+        [HttpPost]
+        public IActionResult create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            var itemToBeDeleted = _context.Items.Where(data => data.Id == Id).FirstOrDefault();
+            _context.Items.Remove(itemToBeDeleted);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
